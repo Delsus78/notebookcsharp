ed6acdbebd1d195a6131dc5cd0b032e6d8cdebb6
Anthony Paboeuf <62709270+Delsus78@users.noreply.github.com>
2021-03-10 12:18:02 +0100

    Add files via upload

---
+++ NoteBook/Logic/PedagogicalElement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Logic
+{
+    [DataContract]
+    public class PedagogicalElement
+    {
+
+        [DataMember] private float coef;
+        [DataMember] private string name;
+
+        /// <summary>
+        /// Renvoi une exception si la valeur est <= 0
+        /// </summary>
+        public float Coef
+        {
+            get => coef;
+            set
+            {                
+                if (value <= 0) throw new Exception("The coef must be > 0");
+                coef = value;
+            }
+        }
+
+        /// <summary>
+        /// Renvoi une exception si la valeur est vide ou null
+        /// </summary>
+        public string Name
+        {
+            get => name;
+            set
+            {
+                //if (value == null || value == "") Correction, il existe une méthode pour faire cela
+                if (String.IsNullOrEmpty(value)) throw new Exception("The name can't be empty");
+                name = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            //return this.name + " ("+this.coef+")"; Conseil, utiliser un formateur pour simplifier la lecture de la méthode
+            return String.Format("{0} ({1})", this.Name, this.Coef);
+        }
+
+        public bool Equals(PedagogicalElement pe)
+        {
+            bool res = true;
+
+            if (pe.ToString() != this.ToString()) res = false;// Ca ne garantie pas l'égalité, c'est un hack, comparer terme à terme
+
+            return res;
+        }
+    }
+}
---
+++ NoteBook/Logic/Unit.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Logic
+{
+    [DataContract]
+    public class Unit : PedagogicalElement
+    {
+        [DataMember] private List<Module> modules = new List<Module>();
+
+
+        /// <summary>
+        /// Renvoi une exception si la valeur est null
+        /// </summary>
+        public List<Module> Modules
+        {
+            get => modules;
+            set
+            {
+                if (value == null) throw new Exception("The teacher can't be empty");
+                modules = value;
+            }
+        }
+
+        /// <summary>
+        /// Renvoie une liste de modules
+        /// </summary>
+        /// <returns>une liste de modules</returns>
+        public Module[] ListModules()
+        {
+            return this.modules.ToArray();
+        }
+
+        /// <summary>
+        /// ajoute un module a la liste modules
+        /// </summary>
+        public void AddModule(Module module)
+        {
+            this.modules.Add(module);
+        }
+
+        /// <summary>
+        /// supprime un module de la liste modules
+        /// </summary>
+        public void RemoveModule(Module module)
+        {
+            this.modules.Remove(module);
+        }
+    
+        /// <summary>
+        /// calcule la moyenne de tout les modules de cette UE
+        /// </summary>
+        /// <param name="exams">liste d'exam</param>
+        /// <returns></returns>
+        public AvgScore[] ComputeAverages(Exam[] exams)
+        {
+            List<AvgScore> res = new List<AvgScore>();
+
+            float avgRes = 0;
+            float avgDiv = 0;
+
+            foreach (Module m in ListModules())
+            {
+                AvgScore moduleAvg = m.ComputeAverage(exams);
+
+                if (moduleAvg != null)
+                {
+                    avgRes += moduleAvg.Element.Coef * moduleAvg.Average;
+                    avgDiv += moduleAvg.Element.Coef;
+
+                    res.Add(moduleAvg);
+                }
+            }
+            if (avgDiv != 0)
+            {
+                avgRes = avgRes / avgDiv;
+                res.Insert(0,new AvgScore(avgRes, this));
+            }
+            
+            return res.ToArray();
+        }
+    }
+}
