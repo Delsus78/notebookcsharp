e748b82469565e3292082a7db47d10cd86b8a12e
Anthony Paboeuf <62709270+Delsus78@users.noreply.github.com>
2021-03-10 12:17:33 +0100

    Add files via upload

---
+++ NoteBook/TestLogic/TestPedagogicalElement.cs
@@ -0,0 +1,22 @@
+using Logic;
+using System;
+using Xunit;
+
+namespace TestLogic
+{
+    [TestClass]
+    public class TestPedagogicalElement
+    {
+        [TestMethod]
+        public void TestCoef()
+        {
+            Assert.Throws<Exception>(() => { });
+        }
+
+        [TestMethod]
+        public void TestName()
+        {
+            Assert.Throws<Exception>(() => { });
+        }
+    }
+}
